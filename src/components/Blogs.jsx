07825af05f4e55c44f4b177df26ec302bf069a77
Ashumani <manishkirnapure9@gmail.com>
2025-02-05 00:37:04 +0530

    blogs

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -6,9 +6,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
-import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
-import CommingSoonCanvas from "./canvas/commingsoon";
 
 const ProjectCard = ({
   index,
