61eb41bf3a01c54aaa2f5a5b26380f2bcb541e77
Ashumani <manishkirnapure9@gmail.com>
2025-02-06 03:09:47 +0530

    updat

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,7 +12,7 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-20, 40, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
