6ddb7b87c0db2e45f5c43fcf4ee5d3c382509035
Ashumani <manishkirnapure9@gmail.com>
2025-02-05 19:34:00 +0000

    Deploying to gh-pages from @ Ashumani/Portfolio@03bedbae463e1c3091ffecbaba690d191c217ebd 🚀

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [
-      require('tailwindcss'), // No change here. Tailwind still needs to be included.
-      require('@tailwindcss/postcss'), // This is the crucial addition.
-      require('autoprefixer'), // Often needed alongside Tailwind.
-    ],
-  }
