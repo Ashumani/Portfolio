201c27ee951b8caf06962f1d1b361a44854a7def
Ashumani <manishkirnapure9@gmail.com>
2025-02-08 14:11:44 +0530

    update hom

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,21 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { useEffect, useState } from "react";
+
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+  }, []);
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -25,7 +38,10 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
+
+      {!isMobile ? (<ComputersCanvas />):(
+        <div />
+      )}
       
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
